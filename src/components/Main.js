7b3feb189ecace004e9e58398759780d6faef5a4
TatyCat <williams.tatyanna@gmail.com>
2019-03-05 15:43:32 -0500

    completed refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,15 +7,9 @@ export default function Main(props) {
   const [lightness, setLightness] = useState(1)
   const [alpha, setAlpha] = useState(1)
 
-  //Save Button
+  //Saved Colors
   const [savedColorClicked, setSavedColorClicked] = useState([])
-
-  //Saved Colors Array
-  const savedColors = []
-
-  const savedColorsArray = (hslaValue) => {
-    savedColors.push(hslaValue)
-  }
+  console.log(savedColorClicked)
 
   //Display Saved Colors
 
@@ -37,9 +31,8 @@ export default function Main(props) {
 
   let selectText = event => {
     console.log(event.target.innerText)
-    // event.target.innerText.select()
-    // document.execCommand("copy")
-    // ptag - onClick={selectText} 
+    event.target.innerText.select()
+    document.execCommand("copy")
   }
 
   return (
@@ -48,7 +41,7 @@ export default function Main(props) {
         <section id="color-results" style={{ backgroundColor: `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})` }}></section>
 
         <article>
-          <p id="slider-results">HSLa ({hue}, {saturation}%, {lightness}%, {alpha})</p>
+          <p id="slider-results" onClick={selectText} >HSLa ({hue}, {saturation}%, {lightness}%, {alpha})</p>
           <p>Hue</p>
           <section className="slidecontainer">
             <input onChange={event => setHue(event.target.value)} step="1" type="range" min="0" max="360" value={hue} className="slider" id="hue" />
@@ -70,38 +63,29 @@ export default function Main(props) {
             <input onChange={event => setAlpha(event.target.value)} type="range" step=".1" min="0" max="1" value={alpha} className="slider" id="alpha" />
           </section>
 
+
+          <button style={{
+            boxShadow: `.2px 1px .3px 6px hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha}) inset, .2px 1px 1px 3px hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
+          }}
+            onClick={(event) => setSavedColorClicked(savedColorClicked => savedColorClicked.concat(`hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`))}
+          >Store This Color</button>
+
+
+          <section id="saved-section">
+            <h2>Saved Colors</h2>
+            <div>
+              {savedColorClicked.map((hsla, index) => {
+                return (
+                  <ul key={index}>
+                    <li >{hsla} <span className="saved-color" style={{ backgroundColor: `${hsla}` }}></span>
+                    </li>
+                  </ul>
+                )
+              })}
+            </div>
+          </section>
         </article>
+        <footer></footer>
       </main>
     </>)
 }
-
-/*
-
-
-
-  <button style={{
-    boxShadow: `.2px 1px .3px 6px hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha}) inset, .2px 1px 1px 3px hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha})`
-  }} onClick={
-    setSavedColorClicked(savedColors.push(`hsla(${props.hue}, ${props.saturation}%, ${props.lightness}%, ${props.alpha})`))
-  }>Store This Color</button>
-
-  <section id="saved-section">
-    <h2>Saved Colors</h2>
-    <div>
-      {this.state.savedColors.map((hsla, index) => {
-        return (
-          <ul key={index}>
-            <li >{hsla} <span className="saved-color" style={{ backgroundColor: `${hsla}` }}></span>
-            </li>
-          </ul>
-        )
-      })}
-    </div>
-  </section>
-</article>
-<footer></footer>
-</main >
-</>
-)}} }}
-
-*/
